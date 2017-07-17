4b4300b1198ae496be16ff122404eeecd198e8c3
spaynkee <spaynkee@gmail.com>
2017-07-17 18:35:11 -0500

    Added some codes

--- savSync/Form1.Designer.cs
+++ savSync/Form1.Designer.cs
@@ -28,23 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.lstGames = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnGet = new System.Windows.Forms.Button();
+            this.btnPut = new System.Windows.Forms.Button();
+            this.openGame = new System.Windows.Forms.OpenFileDialog();
             this.SuspendLayout();
             // 
-            // listBox1
+            // lstGames
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(12, 12);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(260, 212);
-            this.listBox1.TabIndex = 0;
+            this.lstGames.FormattingEnabled = true;
+            this.lstGames.Location = new System.Drawing.Point(12, 12);
+            this.lstGames.Name = "lstGames";
+            this.lstGames.Size = new System.Drawing.Size(318, 212);
+            this.lstGames.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 227);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(93, 227);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 23);
+            this.btnRemove.TabIndex = 1;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnGet
+            // 
+            this.btnGet.Location = new System.Drawing.Point(174, 227);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(75, 23);
+            this.btnGet.TabIndex = 1;
+            this.btnGet.Text = "Get";
+            this.btnGet.UseVisualStyleBackColor = true;
+            this.btnGet.Click += new System.EventHandler(this.btnGet_Click_1);
+            // 
+            // btnPut
+            // 
+            this.btnPut.Location = new System.Drawing.Point(255, 227);
+            this.btnPut.Name = "btnPut";
+            this.btnPut.Size = new System.Drawing.Size(75, 23);
+            this.btnPut.TabIndex = 1;
+            this.btnPut.Text = "Put";
+            this.btnPut.UseVisualStyleBackColor = true;
+            this.btnPut.Click += new System.EventHandler(this.btnPut_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.listBox1);
+            this.ClientSize = new System.Drawing.Size(342, 262);
+            this.Controls.Add(this.btnGet);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnPut);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lstGames);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -54,7 +103,12 @@
 
         #endregion
 
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox lstGames;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnGet;
+        private System.Windows.Forms.Button btnPut;
+        private System.Windows.Forms.OpenFileDialog openGame;
     }
 }
 
--- savSync/Form1.cs
+++ savSync/Form1.cs
@@ -6,12 +6,25 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
+using System.Net;
+using FluentFTP;
 
 namespace savSync
 {
+    struct game
+    {
+
+        public string gameName;
+        public string gamePath;
+    }
+
     public partial class Form1 : Form
     {
+        List<game> gameList = new List<game>();
+        string listPath = "C://list.txt";
+
         public Form1()
         {
             InitializeComponent();
@@ -19,14 +32,169 @@ namespace savSync
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //And here is where I'd put my program, if I had one.
-            //On load, we need to check for a list file, parse it in
 
-            //refresh the listbox's display
+            string line;
+            string listGameName;
+            string listGamePath;
 
-            //We need 4 buttons, add remove get put
-            
-            //To submit changes to git, we have to commit changes, then push, then merge
+
+            //if file exists, then parse it. if it doesn't create it and move on.
+            if (File.Exists(listPath) == true)
+            {
+
+                //Read through file, creating a gameList
+                using (StreamReader sw = new StreamReader(listPath))
+                {
+                    while ((line = sw.ReadLine()) != null)
+                    {
+                        //skip any misshappen blank line
+                        if (line == "")
+                        {
+                            continue;
+                        }
+
+                        int indexOfComma = line.IndexOf(",");
+                        int lineLength = line.Length;
+
+                        listGameName = line.Substring(0, indexOfComma);
+                        listGamePath = line.Substring(indexOfComma+1, lineLength - indexOfComma-1);
+
+                        game tempGame = new game();
+                        tempGame.gameName = listGameName;
+                        tempGame.gamePath = listGamePath;
+                        gameList.Add(tempGame);
+                    }
+                }
+
+                //Refresh the listbox with the fancy new gameList
+                refreshListBox();
+            }
+            else
+            {
+                File.CreateText(listPath);
+            }
+
+            //Parse through each line of list file, 
+        }
+
+        public void refreshListBox()
+        {
+            //Clear the listbox
+            lstGames.Items.Clear();
+
+            for (int x = 0; x < gameList.Count(); x++)
+            {
+                lstGames.Items.Add(gameList[x].gameName);
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            // Open a file browser,
+            if (openGame.ShowDialog() == DialogResult.OK)
+            {
+                string selectedFile = openGame.FileName;
+                int indexOfSlash = selectedFile.LastIndexOf("\\");
+
+                string newGameName = selectedFile.Substring(indexOfSlash + 1, selectedFile.Length - indexOfSlash - 1);
+                string newGamePath = selectedFile;
+
+                //Create a new gameObject to store the name/path in so we can add to list
+                game newGame = new game();
+
+                newGame.gameName = newGameName;
+                newGame.gamePath = newGamePath;
+                gameList.Add(newGame);
+
+                //Update the listFile
+                using (StreamWriter sw = File.AppendText(listPath))
+                {
+                    sw.WriteLine(newGame.gameName + "," + newGame.gamePath);
+
+                }
+            }
+
+            //refresh Listbox
+            refreshListBox();
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            //remove the selected item from the gameList
+            gameList.RemoveAt(lstGames.SelectedIndex);
+
+            File.Delete(listPath);
+
+            //Write a new list file
+            using (StreamWriter sw = File.AppendText(listPath))
+            {
+                for (int x = 0; x < gameList.Count; x++)
+                {
+                    sw.WriteLine(gameList[x].gameName + "," + gameList[x].gamePath);
+                }
+
+            }
+            refreshListBox();
+        }
+
+        private void btnGet_Click(object sender, EventArgs e)
+        {
+
+            //Here we get from the FTP server the filename
+        }
+
+        private void btnGet_Click_1(object sender, EventArgs e)
+        {
+            string downloadSavePath = gameList[lstGames.SelectedIndex].gamePath;
+            string downloadSaveName = gameList[lstGames.SelectedIndex].gameName;
+
+            FtpClient client = new FtpClient("192.168.1.3");
+
+            client.Credentials = new NetworkCredential("kitchen", "lalala22");
+
+            client.Connect();
+            //if the file exists on the ftp server, download it to the path recorded in the list
+            if (client.FileExists("//Desktop//FTP//"+downloadSaveName))
+            {
+                bool success = client.DownloadFile(downloadSavePath, "//Desktop//FTP//"+downloadSaveName);
+
+                if (success == true)
+                {
+                    MessageBox.Show("It success");
+                }
+                else
+                {
+                    MessageBox.Show("It failure");
+                }
+            }
+
+            client.Disconnect();
+
+        }
+
+        private void btnPut_Click(object sender, EventArgs e)
+        {
+
+            //gets the file we're gonna ftp
+            string uploadSavePath = gameList[lstGames.SelectedIndex].gamePath;
+            string uploadSaveName = gameList[lstGames.SelectedIndex].gameName;
+
+            FtpClient client = new FtpClient("192.168.1.3");
+
+            client.Credentials = new NetworkCredential("kitchen", "lalala22");
+
+            client.Connect();
+
+            bool success = client.UploadFile(uploadSavePath, "//Desktop//FTP//"+uploadSaveName, FtpExists.Overwrite);
+            if (success == true)
+            {
+                MessageBox.Show("It success");
+            }
+            else
+            {
+                MessageBox.Show("It failure");
+            }
+            client.Disconnect();
         }
     }
-}
+    }
